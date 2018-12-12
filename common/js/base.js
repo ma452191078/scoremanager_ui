6ba2261179cb0ef6c996be17d824fb528daf1e32
Code <majingyuanno.1@gmail.com>
2018-12-12 19:44:05 +0800

    增加比分扣除比例设置

--- common/js/base.js
+++ common/js/base.js
@@ -2,13 +2,13 @@
  * Created by majingyuan on 2017/5/29.
  */
 // 正式系统
-//  var basePath = 'http://weixin.shidanli.cn:8081';
+ var basePath = 'http://weixin.shidanli.cn:8081';
 
 //测试系统
-var basePath = 'http://localhost:8080';
-var projectName = '/scoremanager_ui';
+// var basePath = 'http://localhost:8080';
+var projectName = '/scoremanager_test';
 
-var path = basePath + '/scoremanager';
+var path = basePath + '/scoremanager-t';
 var imgUrl = basePath + '/scoremanager_ui';
 var qrUrl = 'http://weixin.shidanli.cn:8083/?gameId=';
 var loginUrl = "http://" + window.location.host + projectName + "/login.html";
